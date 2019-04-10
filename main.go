230da2736b48c9e4a61b8f2c64c7c9545d7068fd
Shevchenko-26 <shevchenko.diana26@gmail.com>
2019-04-10 23:18:38 +0300

    Add decoding of arguments passed

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"./globbing"
+	"./metrics"
+	"encoding/json"
+	"fmt"
+	"net/url"
+	"os"
+	"time"
+)
+
+func main() {
+
+	address := os.Args[1:][0]
+	project, err := url.QueryUnescape(address)
+
+	files, err := globbing.GetFiles(project)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	result := metrics.Count(files)
+	data, err := json.MarshalIndent(result, "", " ")
+
+	resultDir := project + "/result"
+	_ = os.Mkdir(resultDir, 0755)
+	f, err := os.Create(resultDir + "/result.json")
+	if err != nil {
+		//panic(err)
+		fmt.Println(err)
+	}
+	_, err = f.Write(data)
+	if err != nil {
+		fmt.Println(err)
+		//panic(err)
+	}
+	defer time.Sleep(10 * time.Second)
+}
+
+/*
+func display(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	t, err := template.ParseFiles("layout.html")
+	if err != nil {
+		panic(err)
+	}
+
+	err = t.Execute(w, nil)
+	if err != nil {
+		panic(err)
+	}
+	//fmt.Fprintf(w, "fvfv %v", 0)
+}
+*/
